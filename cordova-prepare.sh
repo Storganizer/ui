7c7a45c24e3e86322e02a46ccc4255b6d2b53538
Claudio <info@gitcd.io>
2023-11-12 10:22:29 +0100

    Check the action

--- cordova-prepare.sh
+++ cordova-prepare.sh
@@ -5,7 +5,7 @@ mkdir -p ./www
 cp ./dist/* ./www
 cp ./public/* ./www
 
-export ANDROID_HOME=/home/claudio/Android/Sdk
+#export ANDROID_HOME=/home/claudio/Android/Sdk
 #export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-11.0.18.0.10-1.fc37.x86_64
 
 rm -rf ./platforms
