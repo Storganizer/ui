bf2c8b3f3524cb925a992f9a534b4c85f707cff1
Claudio <info@gitcd.io>
2023-11-14 17:47:21 +0100

    Local Build system with containers

---
+++ container-pipeline.sh
@@ -0,0 +1,19 @@
+
+rm -rf ./platforms ./plugins
+
+cordova telemetry off
+cordova platform add android
+cordova platform add electron
+cordova build --prod --release --buildConfig=/tmp/cordova-build-config.json
+
+APK=$(find ./ -name *.apk)
+AAB=$(find ./ -name *.aab)
+APP_IMAGE=$(find ./ -name *.AppImage)
+
+echo "Android Package: $APK"
+echo "AppImage: $APP_IMAGE"
+
+cp $APK /tmp/builds/
+cp $AAB /tmp/builds/
+cp $APP_IMAGE /tmp/builds/
+
--- local-build.sh
+++ local-build.sh
@@ -1,8 +1,30 @@
 #!/bin/bash
 
-rm -rf ./www ./dist ./node_modules ./platforms
+echo "Cleanup"
+sudo rm -rf ./builds ./dist ./node_modules ./platforms ./plugins ./www
+mkdir -p ./platforms
+mkdir -p ./www
+mkdir -p ./builds
+
+echo "Fresh install"
 npm install
 npm run production-build
-npm run cordova-prepare
 
-npx cordova build --buildConfig=$(pwd)/cordova-build-config.json
+echo "Copy artifacts"
+cp ./dist/* ./www
+cp ./public/* ./www
+
+echo "Build application formats"
+docker rm -f $(cat version.txt)
+docker run -it --name $(cat version.txt) \
+    -v $(pwd)/www:/tmp/www:z \
+    -v $(pwd)/container-pipeline.sh:/tmp/container-pipeline.sh:z \
+    -v $(pwd)/builds:/tmp/builds:z \
+    -v $(pwd)/config.xml:/tmp/config.xml:z \
+    -v $(pwd)/cordova-build-config.json:/tmp/cordova-build-config.json:z \
+    -v $(pwd)/package.json:/tmp/package.json:z \
+    beevelop/ionic \
+        bash /tmp/container-pipeline.sh
+
+echo "Fixing artifact permissions"
+sudo chown -R claudio: $(pwd)/builds
