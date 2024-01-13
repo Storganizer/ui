658169be4b562ef8daf4e174cc76d1cd2ae36abe
Claudio <info@gitcd.io>
2024-01-13 06:02:04 +0100

    Run with podman

--- build-pipeline/05-cordova-container-build.sh
+++ build-pipeline/05-cordova-container-build.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 echo "Cordova Dev Build"
+mkdir -p $(pwd)/local-builds
 
-docker rm -f $(cat version.txt)
-docker run -it --name $(cat version.txt) \
+podman rm -f $(cat version.txt)
+podman run -it --name $(cat version.txt) \
     -v $(pwd)/www:/tmp/www:z \
     -v $(pwd)/build-pipeline:/tmp/build-pipeline:z \
     -v $(pwd)/local-builds:/tmp/local-builds:z \
